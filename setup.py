bf02338c4ac2aba917c9e5c01b1f0352ba5837eb
jorge.melgarejo <jorge.melgarejo@azi.com.br>
2020-04-13 16:07:38 -0400

    starting project

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='codigo-avulso-test-tutorial',
+    version='0.0.1',
+    url='https://github.com/joorgelm/ruppell',
+    license='MIT License',
+    author='Jorge Melgarejo',
+    author_email='melgarejo.colarte@gmail.com',
+    keywords='ocr text extractor',
+    description=u'text extractor based in tesseract ocr',
+    packages=['ruppel'],
+    install_requires=[],
+)
