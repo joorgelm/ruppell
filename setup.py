bc9732a3146d1dca3ce923fb8a3ed2a631ffee78
Jorge Melgarejo <melgarejo.colarte@gmail.com>
2020-04-22 18:43:01 -0400

    fix setup final

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 PACKAGE_NAME = 'ruppell'
-PACKAGE_DIR = 'src'
-
+PACKAGES = ['', 'readers', 'utils']
+VERSION = '0.0.4'
 DESCRIPTION = "Ruppell is a Python package to help in text extraction from documents."
 
 setup(
-    name='ruppell',
-    version='0.0.3',
+    name=PACKAGE_NAME,
+    version=VERSION,
     url='https://github.com/joorgelm/ruppell',
-    # download_url='https://github.com/joorgelm/ruppell/archive/0.0.3.tar.gz',
-    package_dir={'ruppell': ''},
-    packages=['', 'readers', 'utils'],
+    download_url=f'https://github.com/joorgelm/ruppell/archive/{VERSION}.tar.gz',
+    package_dir={PACKAGE_NAME: ''},
+    packages=PACKAGES,
     license='MIT License',
     author='Jorge Melgarejo',
     author_email='melgarejo.colarte@gmail.com',
